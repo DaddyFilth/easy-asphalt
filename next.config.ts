f90b22d87594885b08448fc3f8f98c902964bce7
DaddyFilth <daddyfilthie@proton.me>
2026-05-04 20:35:04 -0500

    modified:   .env.example 	modified:   .gitignore 	new file:   next.config.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   vercel.json

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from 'next';
+ 
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: 'https://my-legacy-site.com/:path*',
+        },
+      ],
+    };
+  },
+};
+ 
+export default nextConfig;
