0fcc417ba43d732384f46991569f4dcd6f051324
DaddyFilth <daddyfilthie@proton.me>
2026-05-19 11:08:58 -0500

    estimator fix
    fix gui and tools

--- drizzle/0004_final_invoice_costs.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `projects` ADD `additionalCostsJson` text;
-ALTER TABLE `projects` ADD `finalInvoiceTotal` varchar(50);
-ALTER TABLE `projects` ADD `acceptedAt` timestamp NULL;
--- drizzle/0005_remove_password_auth_columns.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `users` DROP COLUMN `loginMethod`;
-ALTER TABLE `users` DROP COLUMN `passwordHash`;
-ALTER TABLE `users` DROP COLUMN `failedLoginAttempts`;
-ALTER TABLE `users` DROP COLUMN `lockedUntil`;
