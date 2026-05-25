f1b30d5f93b99eba4b0600ef095d11663ad294d9
daddyfilth <daddyfilthie@proton.me>
2026-05-25 10:15:25 -0500

    remove dark theme class: make :root always use dark variables

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,9 +8,6 @@ import App from "./App";
 import { getApiUrl } from "./const";
 import "./index.css";
 
-// Force dark mode - app is dark-mode only
-document.documentElement.classList.add("dark");
-
 const queryClient = new QueryClient();
 
 const redirectToEstimatorIfUnauthorized = (error: unknown) => {
